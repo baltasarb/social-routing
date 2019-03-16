4b08c454149966aa2a38d00cbae5f6d614a9d8cc
baltasarb <baltasar.brito@gmail.com>
2019-03-16 12:56:33 +0000

    Add: social routing service folder, with spring boot default project.

---
+++ social-routing-service/src/main/kotlin/ps/g49/socialroutingservice/SocialRoutingServiceApplication.kt
@@ -0,0 +1,11 @@
+package ps.g49.socialroutingservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialRoutingServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialRoutingServiceApplication>(*args)
+}
---
+++ social-routing-service/src/test/kotlin/ps/g49/socialroutingservice/SocialRoutingServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package ps.g49.socialroutingservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SocialRoutingServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
