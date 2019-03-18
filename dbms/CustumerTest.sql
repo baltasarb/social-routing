4c56561776a31abc01b72428886f97170653260a
baltasarb <baltasar.brito@gmail.com>
2019-03-18 15:31:11 +0000

    Update: sql queries to test customer. Add: Person query.

---
+++ dbms/CustumerTest.sql
@@ -0,0 +1,9 @@
+select * from t1.Customer;
+
+drop table t1.customer;
+
+CREATE TABLE t1.Customer (
+	FirstName text,
+	LastName text,
+	Id bigint
+)
---
+++ dbms/SocialRouting.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Person(
+	FirstName text,
+	LastName text,
+	Email text PRIMARY KEY
+);
+
+SELECT FirstName, LastName, Email FROM Person;
