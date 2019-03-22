dbe69cac69bff2228d4f350b6c005bcf3a4db468
baltasarb <baltasar.brito@gmail.com>
2019-03-22 15:43:17 +0000

    Delete: spring data from the project. Add: JDBI to the gradle build.

--- social-routing-service/src/main/kotlin/ps/g49/socialroutingservice/controllers/PersonController.kt
+++ social-routing-service/src/main/kotlin/ps/g49/socialroutingservice/controllers/PersonController.kt
@@ -1,9 +1,14 @@
 package ps.g49.socialroutingservice.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api.sr/persons")
 class PersonController {
+
+    @GetMapping
+    fun findUser() = "User found."
+
 }
