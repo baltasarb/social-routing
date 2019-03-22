f1e5634a17667cc95b53fd7bc64a617381e5d218
baltasarb <baltasar.brito@gmail.com>
2019-03-22 17:05:40 +0000

    Add: database access example using jdbi.

--- social-routing-service/src/main/kotlin/ps/g49/socialroutingservice/controllers/RouteController.kt
+++ social-routing-service/src/main/kotlin/ps/g49/socialroutingservice/controllers/RouteController.kt
@@ -1,10 +1,28 @@
 package ps.g49.socialroutingservice.controllers
 
 import org.springframework.web.bind.annotation.*
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.statement.StatementContext
+import java.lang.RuntimeException
+import java.sql.ResultSet
+
 
 @RestController
 @RequestMapping("/api.sr/routes")
 class RouteController {
 
+    @GetMapping
+    fun findAllRoutes() : String{
 
-  }
+        val jdbi = Jdbi.create("jdbc:postgresql://localhost:5432/SocialRouting", "baltasarb", "baltasarb")
+
+        val routeName : String = jdbi.withHandle<String, RuntimeException> { handle ->
+            handle.createQuery("SELECT Name FROM Route WHERE Name = 'testRoute';")
+                    .mapTo(String::class.java)//when a query returns a single column, we can map it to the desired Java type:
+                    .findOnly()//for a single result
+        }
+
+        return routeName
+    }
+
+}
