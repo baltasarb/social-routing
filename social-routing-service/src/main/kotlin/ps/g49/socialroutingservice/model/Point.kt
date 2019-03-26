31acbc0578281a2941b27cb2a5e81b40d7ab3e19
baltasarb <baltasar.brito@gmail.com>
2019-03-26 11:56:56 +0000

    Delete: point class and related classes.

--- social-routing-service/src/main/kotlin/ps/g49/socialroutingservice/model/Point.kt
+++ social-routing-service/src/main/kotlin/ps/g49/socialroutingservice/model/Point.kt
@@ -2,8 +2,6 @@ package ps.g49.socialroutingservice.model
 
 data class Point(
         private val latitude: Double,
-
         private val longitude: Double,
-
         private val id: Long? = -1
 )
